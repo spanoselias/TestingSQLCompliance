1c451434715ac089b65d516536a49e27c6bcd3dd
spanoselias <spanoselias@outlook.com>
2017-04-08 17:57:08 -0700

    building the structure

---
+++ src/GeneratorEngine.java
@@ -0,0 +1,27 @@
+public class GeneratorEngine
+{
+
+    String tables[] = {"R1", "R2", "R3"};
+
+    public String random_query()
+    {
+        String query=" ";
+
+        return query;
+    }
+
+
+    public String query()
+    {
+
+        String query=" ";
+
+        return query;
+    }
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
