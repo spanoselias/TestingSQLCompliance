3f6306048fe761fecae583ccce9d17f9c746dc54
elias spanos <spanoselias@outlook.com>
2017-05-29 12:13:05 +0100

    Commenting SQLGenerator class

--- src/SQLGenerator.java
+++ src/SQLGenerator.java
@@ -15,18 +15,6 @@ import java.util.Random;
  */
 public class SQLGenerator
 {
-  /*  private SELECT selAccess;
-
-    SQLGenerator( HashMap<String, LinkedList<String>> relAttrIn)
-    {
-          selAccess = new SELECT(true, true, relAttrIn);
-    }
-
-    public  SELECT getGenerator()
-    {
-        return this.selAccess;
-    }*/
-
     public static class SELECT
     {
         private HashMap<String, LinkedList<String>> relAttr;
@@ -38,7 +26,7 @@ public class SQLGenerator
         private String stmFrom = genFrom.getFrom();
 
         //The  which relations are randomly selected
-        private LinkedList<String> frmRels = genFrom.selectedTables;
+        private LinkedList<String> frmRels = genFrom.selectedReltsInFrom;
 
         private HashMap<String, LinkedList<String>> relAttrs = genFrom.getRelAttrs();
 
@@ -102,39 +90,52 @@ public class SQLGenerator
         }
     }
 
+    /**
+     * The class FROM is used to create the FROM clause. The idea is to store
+     * all the relations that will be randomly choose in the FROM clause in order
+     * to pass them in the other classes. For example, we need to know in the SELECT &
+     * WHERE Clause which relations are selected in the FROM clause
+     *
+     */
     public static class FROM
     {
         private Relation rel [];
 
+        //This variable will be used to create the FROM Clause String
         private String fromStm;
 
-        //This HashMap is used to store the associated attributes for each relation. The
+        //This HashMap is used to store all the relations with their associated attributes. The
         //key represents the relation name and the list stores all the attributes for each key (relation)
-        private HashMap<String, LinkedList<String>> relAttrs;
+        private HashMap<String, LinkedList<String>> allRelAttrs;
 
-        LinkedList<String> selectedTables;
+        //This LinkedList will be used to store all the relations that will be randomly chosen
+        //for the FROM Clause
+        private LinkedList<String> selectedReltsInFrom;
+
 
         public FROM( )
         {
 
-            relAttrs = new HashMap<>();
+            allRelAttrs = new HashMap<>();
 
             //This method is used to read all the relations and attributes from the configuration file (config.properties file)
-            //and it stores them in the relationsAttrs hashMap
-            readConfFile(  this.relAttrs);
-
-
+            //and it stores them in the allRelAttrs hashMap
+            readConfFile(  this.allRelAttrs);
 
             fromStm = "";
 
-            rel = new Relation [this.relAttrs.size()];
+
+            //We copy all the relations from the LinkedList to an array in order to
+            //shuffle the array and afterward to choose some random relation to create
+            //the FROM Clause
+            rel = new Relation [this.allRelAttrs.size()];
             int i=0;
-            for (String relation : this.relAttrs.keySet()) {
+            for (String relation : this.allRelAttrs.keySet()) {
                 rel[i] = new Relation();
 
                 rel[i].setRelName(relation);
 
-                for (String attr : this.relAttrs.get(relation))
+                for (String attr : this.allRelAttrs.get(relation))
                 {
                     rel[i].setAttrName(attr);
                 }
@@ -144,25 +145,30 @@ public class SQLGenerator
 
             //This list will be used to track which relations have been selected from the generator. Thus, it will be useful
             //to know the relations in the FROM statement in order to know what attributes to include in the SELECT statement
-            selectedTables = new LinkedList<>();
+            selectedReltsInFrom = new LinkedList<>();
         }
 
         private String getFrom()
         {
-            //Clear linkedlist
-            selectedTables.clear();
+            //Clear LinkedList
+            selectedReltsInFrom.clear();
 
             Random randomGenerator = new Random();
 
             int pickRand;
-            pickRand = (randomGenerator.nextInt(rel.length) % rel.length) + 1;
+
+            //This random number indicates how many relation the FROM Clause will have
+            pickRand = (randomGenerator.nextInt(rel.length) % rel.length);
+
+            //We shuffle the array of the relations to avoid choosing always the same order.
             shuffleArray(rel);
+
             String stm = "FROM";
 
             for (int i = 0; i < pickRand; i++)
             {
-                this.selectedTables.add(rel[i].getRelName());
-                this.relAttrs.put(rel[i].getRelName(), rel[i].getRelAttrs());
+                this.selectedReltsInFrom.add(rel[i].getRelName());
+                this.allRelAttrs.put(rel[i].getRelName(), rel[i].getRelAttrs());
 
                 if (i == 0)
                     stm += String.format(" %s AS %s", rel[i].getRelName(), rel[i].getRelName().toLowerCase());
@@ -180,11 +186,11 @@ public class SQLGenerator
         }
 
         private LinkedList<String> getTables() {
-            return this.selectedTables;
+            return this.selectedReltsInFrom;
         }
 
         private HashMap<String, LinkedList<String>> getRelAttrs() {
-            return this.relAttrs;
+            return this.allRelAttrs;
         }
     }
 
@@ -198,56 +204,85 @@ public class SQLGenerator
         return pickRand;
     }
 
-    public static class WHERE
-    {
-        private String stm;
-        private COMPARISON genCom;
-        private int whereNo=-1;
-        private String conn[] = {" AND", " OR"};
 
-        public WHERE(HashMap<String, LinkedList<String>> relAttrs, LinkedList<String> selectedTablesIn)
+    /**
+     * The class WHERE is used to create the WHERE clause. In order to know
+     * which attributes are valid to use the WHERE clause, we have a Linkedlist that
+     * stores all the relations that are part of the FROM clause
+     *
+     */
+        public static class WHERE
         {
-            stm = "WHERE ";
+            private String stm;
+            private COMPARISON genCom;
+            private int whereNo=-1;
+            private String conn[] = {" AND", " OR"};
 
-            genCom = new COMPARISON(relAttrs, selectedTablesIn);
-        }
 
-        public WHERE(HashMap<String, LinkedList<String>> relAttrs, LinkedList<String> selectedTablesIn, int whereNoComp)
-        {
-            stm = "WHERE ";
-            genCom = new COMPARISON(relAttrs, selectedTablesIn);
-
-            //The whereNo variable indicates the number of connectivities such as "AND", "OR"
-            //we will have the WHERE sql statement
-            this.whereNo = whereNoComp;
-        }
-
-        private String getSqlWhere()
-        {
-            if(whereNo != -1)
+            /**
+             *The WHERE constructor takes two parameters. The first one (allReltsWithAttrs) has all the relations
+             *that our database schema has with the associated attributes for each relation. The second parameter
+             *(selectedReltsInFrom)  is a linkedlist and it stores all the relations which are randomly choose for
+             * the FROM CLAUSE.
+             *
+             *@param allReltsWithAttrs ,it stores all the relations with their accosiated attributes
+             *@param selectedReltsInFrom ,it stores all the relations that have choose for the FROM CLAUSE
+             */
+            public WHERE(HashMap<String, LinkedList<String>> allReltsWithAttrs, LinkedList<String> selectedReltsInFrom)
             {
-                for(int i=0; i< (whereNo +1) ; i++)
+                stm = "WHERE ";
+
+                genCom = new COMPARISON(allReltsWithAttrs, selectedReltsInFrom);
+            }
+
+
+            /**
+             *The WHERE constructor takes two parameters. The first one (allReltsWithAttrs) has all the relations
+             *that our database schema has with the associated attributes for each relation. The second parameter
+             *(selectedReltsInFrom)  is a linkedlist and it stores all the relations which are randomly choose for
+             * the FROM CLAUSE.
+             *
+             *@param allReltsWithAttrs ,it stores all the relations with their accosiated attributes
+             *@param selectedReltsInFrom ,it stores all the relations that have choose for the FROM CLAUSE
+             *@param whereNoComp ,it is an integer number which indicates how many connectivities (AND, OR ...) we will
+             *have in the WHERE clause
+             */
+            public WHERE(HashMap<String, LinkedList<String>> allReltsWithAttrs, LinkedList<String> selectedReltsInFrom, int whereNoComp)
+            {
+                stm = "WHERE ";
+                genCom = new COMPARISON(allReltsWithAttrs, selectedReltsInFrom);
+
+                //The whereNo variable indicates the number of connectivities such as "AND", "OR"
+                //we will have the WHERE sql statement
+                this.whereNo = whereNoComp;
+            }
+
+            private String getSqlWhere()
+            {
+                if(whereNo != -1)
                 {
-                    if(i==0)
+                    for(int i=0; i< (whereNo +1) ; i++)
                     {
-                        stm += genCom.getAttrComparison();
-                    }
-                    else
-                    {
-                        stm += conn[genRandNo(conn.length)] + " " + genCom.getAttrComparison();
+                        if(i==0)
+                        {
+                            stm += genCom.getAttrComparison();
+                        }
+                        else
+                        {
+                            stm += conn[genRandNo(conn.length)] + " " + genCom.getAttrComparison();
+                        }
                     }
                 }
+
+                else
+                {
+                    stm += genCom.getAttrComparison();
+                }
+
+                return stm;
+
             }
-
-            else
-            {
-                stm += genCom.getAttrComparison();
-            }
-
-            return stm;
-
         }
-    }
 
     public static class COMPARISON
     {
@@ -271,9 +306,16 @@ public class SQLGenerator
             this.operators.add("<>");
         }
 
+
+        /**
+         * This method generate numbers between 0..inputSize
+         *We can use this number in order to choose randomly relations or attributes
+         *Thus, the inputSize can be the total number of relations or attributes
+         *
+         *@param inputSize is the image that represents the NORTH Direction
+         */
         private static int genRandNo(int inputSize)
         {
-
             Random randomGenerator = new Random();
 
             int pickRand = (randomGenerator.nextInt(inputSize) % inputSize);
@@ -281,7 +323,8 @@ public class SQLGenerator
             return pickRand;
         }
 
-        private String getAttrComparison() {
+        private String getAttrComparison()
+        {
             Random randomGenerator = new Random();
 
             int pickRand;
