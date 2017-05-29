c01558b13fbab28883165634a89bb8a4680df956
elias spanos <spanoselias@outlook.com>
2017-05-29 15:45:06 +0100

    Clearing the code

--- src/SQLGenerator.java
+++ src/SQLGenerator.java
@@ -25,7 +25,6 @@ public class SQLGenerator
         SELECT query;
 
 
-
         public GenQuery(boolean isDistinctIn, boolean isAttrsIn, int whereNo )
         {
             query = new SELECT(isDistinctIn, isAttrsIn,whereNo, alias);
@@ -439,48 +438,6 @@ public class SQLGenerator
 
             return rel1 + " " + oper + " " + rel2;
         }
-    }
-
-
-    public static char[] genAttr(int startCode, int endCode) {
-        char[] attr = new char[57];
-
-        int k = 0;
-        for (int i = 0; i < 26; i++) {
-            attr[i] = (char) (65 + (k++));
-        }
-        return attr;
-    }
-
-    public static String query() {
-
-        Relation relations[] = new Relation[10];
-        char attr[] = genAttr(1, 2);
-
-        String rel = "R";
-
-        for (int i = 0; i < 5; i++) {
-            //Initialization & allocation of memory for each object relation
-            relations[i] = new Relation();
-
-            String buildRel = rel + (i + 1);
-            relations[i].relName = buildRel;
-
-            for (int j = 0; j < 5; j++) {
-                relations[i].attributes.add(Character.toString(attr[j]));
-            }
-        }
-
-        String tables[] = {"R1", "R2", "R3", "R4", "R5", "R6"};
-
-        int randTable = 0;
-
-        Random randomGenerator = new Random();
-
-        randTable = randomGenerator.nextInt(tables.length);
-
-
-        return tables[randTable];
     }
 
     public static void shuffleArray(Relation[] a) {
