47881e3f450637df1aeb893d7882ac058201d479
elias spanos <spanoselias@outlook.com>
2017-06-05 21:34:56 +0100

    Implementing script for mysql

---
+++ scripts/mySQL.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS
+R1,
+R2,
+R3,
+R4,
+R5;
+
+CREATE TABLE R1 ( -- df: size=4
+       A smallint, -- df: size=2
+       B smallint, -- df: size=2
+       UNIQUE(A,B)
+);
+
+CREATE TABLE R2 ( -- df: size=4
+       A smallint, -- df: size=2
+       B smallint, -- df: size=2
+       UNIQUE(A,B)
+);
+
+CREATE TABLE R3 ( -- df: size=4
+       A smallint, -- df: size=2
+       B smallint, -- df: size=2
+       UNIQUE(A,B)
+);
+
+CREATE TABLE R4 ( -- df: size=4
+       A smallint, -- df: size=2
+       B smallint, -- df: size=2
+       UNIQUE(A,B)
+);
+
+CREATE TABLE R5 ( -- df: size=4
+       A smallint, -- df: size=2
+       B smallint, -- df: size=2
+       UNIQUE(A,B)
+);
