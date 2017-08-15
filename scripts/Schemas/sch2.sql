150f11c953a4ed615b2045134e24a5ea61e38ee8
elias spanos <spanoselias@outlook.com>
2017-08-15 23:46:25 +0100

    #Commenting the code

---
+++ scripts/Schemas/sch2.sql
@@ -0,0 +1,40 @@
+DROP TABLE R1;
+DROP TABLE R2;
+DROP TABLE R3;
+DROP TABLE R4;
+DROP TABLE R5;
+
+CREATE TABLE R1 ( -- df: size=50
+  A smallint NOT NULL, -- df: size=2
+  B smallint NOT NULL, -- df: size=2
+  C CHAR(22) NOT NULL,
+  D VARCHAR (22) NOT NULL
+);
+
+CREATE TABLE R2 ( -- df: size=50
+  A smallint NOT NULL, -- df: size=2
+  B smallint NOT NULL, -- df: size=2
+  C CHAR(22) NOT NULL,
+  D VARCHAR (22) NOT NULL
+);
+
+CREATE TABLE R3 ( -- df: size=50
+  A smallint NOT NULL, -- df: size=2
+  B smallint NOT NULL, -- df: size=2
+  C CHAR(22) NOT NULL,
+  D VARCHAR (22) NOT NULL
+);
+
+CREATE TABLE R4 ( -- df: size=50
+  A smallint NOT NULL, -- df: size=2
+  B smallint NOT NULL, -- df: size=2
+  C CHAR(22) NOT NULL,
+  D VARCHAR (22) NOT NULL
+);
+
+CREATE TABLE R5 ( -- df: size=50
+  A smallint NOT NULL, -- df: size=2
+  B smallint NOT NULL, -- df: size=2
+  C CHAR(22) NOT NULL,
+  D TEXT  NOT NULL
+);
